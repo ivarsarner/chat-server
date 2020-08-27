12e7f17f4bbfce268ac68f9936ed586aaff1ccdf
ivarsarner <ivar.sarner@appliedtechnology.se>
2020-08-27 19:49:45 +0200

    adding ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('oh hi mark')
+import express from 'express';
+import http from 'http'
+import socket from 'socket.io'
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const port = process.env.PORT || 8080
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+});
+
+server.listen(port, () => {
+  console.log(`Socket IO server listening on ${port}`);
+});
