2b9ebad77a8133d7f5ac6ea19d982a355d702564
ivarsarner <ivar.sarner@appliedtechnology.se>
2020-09-01 17:58:53 +0200

    fixed error handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
 import http from 'http';
 import cors from 'cors';
 
 import { initSocketIoServer } from './socket';
+import { logger } from './logger';
 
 const app = express();
 const server = http.createServer(app);
@@ -17,6 +18,15 @@ app.get('/', (req, res) => {
 
 server.listen(port, () => {
   console.log(`Server listening on ${port}`);
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error(req.url);
+  logger.error(err.message, err);
+  res.status(400).json({
+    error: err.message,
+  });
+  next();
 });
 
 initSocketIoServer(server);
